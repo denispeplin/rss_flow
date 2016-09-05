61d4e79ab4d3af83d5e9db808a3f231bd0d91392
denispeplin <denis.peplin@gmail.com>
2016-09-05 11:56:25 +0300

    Implement convertor from XmlParser format into RSS-specific format.

--- lib/rss_flow.ex
+++ lib/rss_flow.ex
@@ -1,2 +1,56 @@
 defmodule RssFlow do
+  @moduledoc """
+  This module uses output provided by XmlParser, transforms it to RSS-specific
+  format, filters it, and transforms to format that can be consumed by
+  XmlBuilder
+  """
+
+  @doc """
+  Transform XmlParser format to RSS-specific format
+  """
+  def parse(data) do
+    do_parse data
+  end
+
+  defp do_parse({:rss, attributes, content}) do
+    [channel, items] = do_parse(content)
+
+    %{
+      rss: attributes,
+      channel: channel,
+      items: items
+    }
+  end
+  defp do_parse([{:channel, nil, content}]) do
+    %{false: channel, true: items} = group_items(content)
+    [Enum.into(parse_elements(channel), %{}), parse_elements(items)]
+  end
+
+  defp group_items(data) when is_list(data) do
+    Enum.group_by data, fn(tuple) -> elem(tuple, 0) == :item end
+  end
+
+  defp parse_elements([head | tail]) do
+    [parse_elements(head) | parse_elements(tail)]
+  end
+  defp parse_elements({:item, nil, item}) do
+    parse_elements(item) |> Enum.into(%{})
+  end
+  defp parse_elements([]), do: []
+  defp parse_elements({name, nil, value}), do: {name, value}
+  defp parse_elements({name, attributes, value}) do
+    {name, Map.merge(attributes, %{value: value})}
+  end
+
+  @doc """
+  Filter RSS items.
+  """
+  def filter do
+  end
+
+  @doc """
+  Transforms RSS-specific format to XmlBuilder format
+  """
+  def generate do
+  end
 end
--- test/rss_flow_test.exs
+++ test/rss_flow_test.exs
@@ -1,8 +1,53 @@
 defmodule RssFlowTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest RssFlow
+  alias RssFlow
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  def xml_data do
+    {:rss, %{version: "2.0"},
+     [{:channel, nil,
+       [{:title, nil, "Awesome Job"},
+        {:description, nil, "Most awesome jobs in the world!"},
+        {:link, nil, "http://example.com"},
+        {:item, nil,
+         [{:title, nil, "Ruby developer"},
+          {:description, nil, "Remote position. Mostly backend stuff."},
+          {:guid, %{isPermaLink: "true"}, "http://example.com/jobs/1"}]},
+        {:item, nil,
+         [{:title, nil, "Backend Elixir developer"},
+          {:description, nil, "Onsite position"},
+          {:guid, %{isPermaLink: "true"}, "http://example.com/jobs/4"}]}]}]}
+  end
+
+  def rss_data do
+    %{
+      rss: %{version: "2.0"},
+      channel: %{
+        title: "Awesome Job",
+        description: "Most awesome jobs in the world!",
+        link: "http://example.com"
+      },
+      items: [%{
+          title: "Ruby developer",
+          description: "Remote position. Mostly backend stuff.",
+          guid: %{
+            value: "http://example.com/jobs/1",
+            isPermaLink: "true"
+          }
+        },
+        %{
+          title: "Backend Elixir developer",
+          description: "Onsite position",
+          guid: %{
+            value: "http://example.com/jobs/4",
+            isPermaLink: "true"
+          }
+        }]}
+  end
+
+  describe "parse/1" do
+    test "transforms data to internal format" do
+      assert RssFlow.parse(xml_data) == rss_data
+    end
   end
 end
